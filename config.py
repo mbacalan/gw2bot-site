db8886356d543e1e995bde4d9f8829c11df612a2
Maselkov <Maselkov@users.noreply.github.com>
2017-08-08 23:47:07 +0200

    Add OAUTH2 authentication to Discord

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SECRET_KEY = ""
+SECRET_KEY = "OAUTH_SECRET"
 DEBUG = True
 OAUTH2_CLIENT_ID = ""
 BOT_INVITE_LINK = "https://discordapp.com/oauth2/authorize?client_id=310050883100737536&scope=bot&permissions=27648"
