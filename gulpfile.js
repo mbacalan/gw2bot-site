d91958f98379a7801fed3d54fe1e6d73c9f7b6d9
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-04-05 20:41:38 +0300

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+/* eslint-env node */
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+var prefixer = require("gulp-autoprefixer");
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var exec = require("child_process").exec;
+
+// DEVELOPMENT
+
+//Run Flask server
+gulp.task("runServer", function () {
+  exec("python gw2botsite.py");
+});
+
+//Transpile SCSS & add prefixes
+gulp.task("styles", function () {
+  gulp
+    .src("static/sass/**/*.scss")
+    .pipe(sass()).on("error", sass.logError)
+    .pipe(prefixer({ browsers: ["last 2 versions"] }))
+    .pipe(gulp.dest("static/css"));
+});
+
+//Initialize browserSync
+gulp.task("browserSync", function () {
+  browserSync.init({
+    proxy: "localhost:5000",
+    //Change this to your local IP or remove it for automatic detection
+    host: "192.168.2.202"
+  });
+});
+
+// Default task: Watch Files For Changes & Reload browser
+gulp.task("default", ["runServer", "styles", "browserSync"], function () {
+  gulp.watch(["templates/**/*", "static/**/*"]).on("change", browserSync.reload);
+  gulp.watch("static/sass/*.scss", ["styles"]);
+});
+
+// PRODUCTION
+
+//Copy files to dist/
+gulp.task("copyPy", function () {
+  gulp
+    .src(["gw2botsite.py", "config.py"])
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("copyTemplates", function() {
+  gulp
+    .src("templates/**/*")
+    .pipe(gulp.dest("dist/templates"));
+});
+
+gulp.task("copyImg", function() {
+  gulp
+    .src(["static/img/*"])
+    .pipe(gulp.dest("dist/static/img"));
+});
+
+//Minify CSS and write sourcemaps for production
+gulp.task("stylesDist", function () {
+  gulp
+    .src("static/sass/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" })).on("error", sass.logError)
+    .pipe(prefixer({ browsers: ["last 2 versions"] }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/static/css"));
+});
+
+//Minify JS for production
+gulp.task("minify", function () {
+  gulp
+    .src("static/js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/static/js"));
+});
+
+//Put everything together for production
+gulp.task("dist", ["copyPy", "copyTemplates", "copyImg", "stylesDist", "minify"]);
