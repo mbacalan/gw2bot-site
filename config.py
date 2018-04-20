879f5887df71af6a64d347a920ea6d7b53e33895
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-04-20 18:38:31 +0300

    Clean up/delete redundant files

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 SECRET_KEY = "OAUTH_SECRET"
 DEBUG = True
 OAUTH2_CLIENT_ID = ""
-BOT_INVITE_LINK = "https://discordapp.com/oauth2/authorize?client_id=310050883100737536&scope=bot&permissions=402746368"
-BOT_SUPPORT_SERVER_LINK = "https://discord.gg/VyQTrwP"
