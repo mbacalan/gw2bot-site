e7a31b6cc20b52601ece625941c421267c6d16cb
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-11-17 18:33:24 +0300

    fix: Banner too small for 2K resolutions
    The new banner is added seperately so it doesn't affect any
    other resolutions and keeps the page load time the same.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="header">
     <picture>
+      <source media="(min-width: 2550px)" srcset="@/assets/img/banner1440p.png">
       <source media="(min-width: 1024px)" srcset="@/assets/img/banner.png">
       <img class="header-img" src="@/assets/img/avatarwithtext.png" alt="Banner with GW2Bot Logo">
     </picture>
