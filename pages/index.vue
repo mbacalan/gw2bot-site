dca33ab1828b216c942f33f6c12b88fa4d31b826
SugarcubesLaptop <Kimberly.VandenBroeck@hotmail.com>
2019-10-07 18:46:13 +0200

    added stats of the most used commands, commented out the gif

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,16 @@
     </h1>
     <a class="button" href="https://discordapp.com/oauth2/authorize?client_id=310050883100737536&scope=bot&permissions=939879488">Add To Server!</a>
     <hr>
-    <picture>
+
+<h3>▓▓▓▓▓▓░░░░░░░░░░░░░░ 29% used bosses
+</h3>
+<h3>▓▓░░░░░░░░░░░░░░░░░░ 10% used li</h3>
+<h3>▓▓░░░░░░░░░░░░░░░░░░ 9% used search</h3>
+
+    <!-- <picture>
       <source media="(min-width: 1024px)" srcset="@/assets/img/botdemo.gif">
       <img src="@/assets/img/botdemo-sm.gif" alt="A .gif demonstration of the bot">
-    </picture>
+    </picture> -->
   </div>
 </template>
 
