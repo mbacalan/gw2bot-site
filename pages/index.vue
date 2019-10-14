1fadb7926b6a9b7cc11346f6c5afc98db9f4d9de
SugarcubesLaptop <Kimberly.VandenBroeck@hotmail.com>
2019-10-14 11:51:25 +0200

    commented out code and linked img

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="index">
-    <hr>
+    <!-- <hr> -->
     <h1 class="headline">
       Bring the world of Tyria into Discord
     </h1>
     <a class="button" href="https://discordapp.com/oauth2/authorize?client_id=310050883100737536&scope=bot&permissions=939879488">Add To Server!</a>
-    <hr>
-<h3><span>▓▓▓▓▓▓</span>░░░░░░░░░░░░░░ 29% used bosses</h3>
-<h3><span>▓▓</span>░░░░░░░░░░░░░░░░░░ 10% used li</h3>
-<h3><span>▓▓</span>░░░░░░░░░░░░░░░░░░ 9% used search</h3>
+    <!-- <hr> -->
+
+  <img class="most-used" src="@/assets/img/mostUsedCommand2.png" alt="most used commands">
+
+  <!-- <h3><span>▓▓▓▓▓▓</span>░░░░░░░░░░░░░░ 29% used bosses</h3>
+  <h3><span>▓▓</span>░░░░░░░░░░░░░░░░░░ 10% used li</h3>
+  <h3><span>▓▓</span>░░░░░░░░░░░░░░░░░░ 9% used search</h3> -->
   </div>
 </template>
 
