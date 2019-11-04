fd0d756d0a8c921496a55905441f48c5cd9b0f99
SugarcubesLaptop <Kimberly.VandenBroeck@hotmail.com>
2019-11-04 19:40:02 +0100

    removed  empty space

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="index">
-
     <h1 class="headline">
       Bring the world of Tyria into Discord
     </h1>
     <a class="button" href="https://discordapp.com/oauth2/authorize?client_id=310050883100737536&scope=bot&permissions=939879488">Add To Server!</a>
 
-  <img class="most-used" src="@/assets/img/mostUsedCommand2.png" alt="most used commands">
-
+    <img class="most-used" src="@/assets/img/mostUsedCommand2.png" alt="most used commands">
   </div>
 </template>
 
