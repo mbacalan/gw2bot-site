5cbe3fbcef633c6fcd57bb5a3ae129eabda4ba05
Matt Bryant <brybrant@gmail.com>
2022-08-04 13:52:53 +0100

    Added new privacy policy (copied from live website)

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,95 @@
+<template>
+  <main>
+    <div class="page-width page-padding">
+      <header>
+        <h1>GW2Bot Privacy Policy</h1>
+
+        <p>Last Updated: July 13, 2022</p>
+      </header>
+
+      <p class="small-text italic-text">
+        GW2Bot is an open-source interactive software tool (&ldquo;bot&rdquo;) created for use exclusively with the <a class="external" href="https://discord.com/" target="_blank">Discord</a> application, and is designed to provide helpful features that compliment <a class="external" href="https://www.guildwars2.com" target="_blank">Guild&nbsp;Wars&nbsp;2</a> (<abbr>GW2</abbr>)&nbsp;gameplay.
+      </p>
+
+      <p>We are committed to protecting your&nbsp;privacy. We understand that you do not want the data you provide to GW2Bot to be distributed indiscriminately and this privacy policy explains how we collect data, what we do with it and what controls you have over&nbsp;it. All of the data we acquire from you is collected and processed in accordance with General Data Protection Regulations&nbsp;(GDPR). <strong>We don&rsquo;t share or sell any of your data to anyone and we&nbsp;never&nbsp;will.</strong></p>
+
+      <p>By using GW2Bot, you consent to the collection of data about your usage of GW2Bot in accordance with this privacy&nbsp;policy. If you use GW2Bot features that require authentication via <dfn><abbr title="Guild Wars 2">GW2</abbr></dfn> access token <em>(hereafter referred to as an&nbsp;&ldquo;<abbr>API</abbr>&nbsp;key&rdquo;),</em> then you also consent to the collection and use of your <abbr>GW2</abbr>&nbsp;gaming data in accordance with this privacy&nbsp;policy. If you do not agree with the use of your data as described in this privacy policy, please do not use&nbsp;GW2Bot. If you do not agree with the use of your <abbr>GW2</abbr> gaming data as described in this privacy policy, please do not use GW2Bot features that require authentication. <strong>We do not collect any personal data about you such as your name, your age, pictures of your cat, or any other personally identifiable&nbsp;information.</strong></p>
+
+      <p>GW2Bot requires access to Discord message content to enable <a class="external" href="https://wiki.guildwars2.com/wiki/Chat_link_format" target="_blank"><abbr>GW2</abbr> chat link</a> functionality within&nbsp;Discord. This means that GW2Bot can see your messages, but it cannot <em>&ldquo;read&rdquo;</em> or understand your messages; GW2Bot only searches new messages for a&nbsp;valid <abbr>GW2</abbr> chat link and then responds with the appropriate <abbr>GW2</abbr> game element if one is found, otherwise the message is disregarded. You can disable this feature by using the <code>/server preview_chat_links</code> command. <strong>GW2Bot does not record your message content and never&nbsp;will.</strong></p>
+
+      <p>We reserve the right to change this privacy policy from time to&nbsp;time. If we make any changes to this privacy policy, we will notify you by means of an announcement on the <a class="external" :href="$supportServerLink" target="_blank">GW2Bot Discord Server</a> prior to the changes becoming effective, which will give you the opportunity to review the changes before you choose to continue using&nbsp;GW2Bot. You will also receive a direct message about the changes from GW2Bot when you first interact with GW2Bot after this privacy policy is&nbsp;updated. You can always find a link to this privacy policy in the footer of every page on this website, or by using GW2Bot&rsquo;s <code>/help</code>&nbsp;command.</p>
+
+      <h2 id="what-data-do-we-collect" class="anchor">
+        What Data Do We&nbsp;Collect?
+      </h2>
+
+      <p>The data collected by GW2Bot depends on the context of your interactions with GW2Bot and the features you&nbsp;use. GW2Bot automatically collects your usage&nbsp;data for analytical and diagnostic purposes when you interact with&nbsp;GW2Bot.</p>
+
+      <p>GW2Bot may also collect and process data that you voluntarily provide, such as your GW2Bot preferences and your <abbr>GW2</abbr> <abbr>API</abbr>&nbsp;key via the&nbsp;<code>/key add</code> command. The data you provide to GW2Bot is securely stored and associated with your Discord User&nbsp;<abbr>ID</abbr> to provide a personalised&nbsp;experience. <a href="#what-are-your-data-protection-rights">Learn more about your data protection&nbsp;rights.</a></p>
+
+      <h2 id="how-will-we-use-your-data" class="anchor">
+        How Will We Use Your&nbsp;Data?
+      </h2>
+
+      <p>GW2Bot may use the information that we collect <em>(subject to your usage of GW2Bot)</em> as described&nbsp;below:</p>
+
+      <ul>
+        <li><strong>To improve GW2Bot:</strong> We may use your error logs and general usage data to help in debugging, fixing errors, and making improvements to&nbsp;GW2Bot.</li>
+
+        <li><strong>To provide a personalised experience:</strong> We may use your <abbr>GW2</abbr> <abbr>API</abbr>&nbsp;key to fetch data from <a class="external" href="https://wiki.guildwars2.com/wiki/API:Main#Authentication" target="_blank">authenticated <abbr>GW2</abbr> <abbr>API</abbr> endpoints</a> to provide an experience that is unique and relevant to&nbsp;you. Some examples include searching for an item on your <abbr>GW2</abbr> account, checking your weekly raid progression, and synchronizing your in-game Guild&nbsp;Ranks with Roles in your Discord&nbsp;Server.</li>
+
+        <li><strong>To provide convenience and quality of life&nbsp;features:</strong> We may store your GW2Bot preferences to provide convenience and quality of life features such as sending you opt-in notifications for game events, gem price changes, and other special occurrences such as Daily Mystic&nbsp;Forger.</li>
+      </ul>
+
+      <h2 id="data-retention" class="anchor">
+        Data Retention
+      </h2>
+
+      <p>GW2Bot securely stores <a href="#what-data-do-we-collect">the aforementioned data</a> until it is no longer necessary, or until you request us to erase it &ndash; whichever comes&nbsp;first. This is a case&#8209;by&#8209;case determination that depends on things such as the nature of the data, why it is collected and processed, and relevant legal or operational retention needs.</p>
+
+      <p><strong>When you request us to erase your data, we erase all of your data, and you won&rsquo;t be able to recover this information later.</strong></p>
+
+      <h2 id="what-are-your-data-protection-rights" class="anchor">
+        What Are Your Data Protection Rights?
+      </h2>
+
+      <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the&nbsp;following:</p>
+
+      <ul>
+        <li><strong>The right to access:</strong> You have the right to request copies of your&nbsp;data.</li>
+
+        <li><strong>The right to rectification:</strong> You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete any information you believe is&nbsp;incomplete.</li>
+
+        <li><strong>The right to erasure:</strong> You have the right to request that we erase your&nbsp;data.</li>
+
+        <li><strong>The right to restrict processing:</strong> You have the right to request that GW2Bot restrict the processing of your data, under certain conditions.</li>
+
+        <li><strong>The right to object to processing:</strong> You have the right to object to GW2Bot processing your data, under certain conditions.</li>
+
+        <li><strong>The right to data portability:</strong> You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</li>
+      </ul>
+
+      <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us via the <a class="external" :href="$supportServerLink" target="_blank">GW2Bot Discord&nbsp;Server</a>.</p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'PrivacyPolicyPage'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/scss/colors';
+@import '~@/assets/scss/settings';
+
+h2.anchor {
+  margin-top: -$nav-height;
+  padding-top: $nav-height + $h2-margin-px;
+}
+p, li {
+  position: relative;
+  z-index: 1;
+}
+</style>
